3803bc17b9ebc4cc22c7ff782bd9b284ff4afcf7
mohameddevopslehcene <mohameddevops1234@gmail.com>
2025-04-15 12:51:03 +0000

    Préparer mon environnement de travail en utilisant React Node JS

---
+++ EMS/frontend/src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+
+
+  return (
+    <div>
+      <h1  className="text-3xl text-teal-500">Welcome to EMS</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ EMS/server/index.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import cors from 'cors'
+
+
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+
+app.listen(process.env.PORT, ()=>{
+    console.log("Server is runing on port : " + process.env.PORT)
+})
+
